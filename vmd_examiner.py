86cbef22d8dcb908d45deea7bda4ee106a07fd9e
Yuto Takamoto <ytakamoto@etsy.com>
2020-04-03 21:07:11 -0400

    whaaaat am I doing

--- vmd_examiner.py
+++ vmd_examiner.py
@@ -1,9 +1,19 @@
 import struct
+from collections import namedtuple
+import sys
+
+if (sys.version_info < (3, 0)):
+  sys.stderr.write("Please run with Py3.x. Unicode and bytes in py2 is a nightmare.\n")
+  exit(1)
 
 # This is literally just me screwing around with figuring out the file structure of a .vmd file.
 
+# Why the fuck is this in CP932. The answer is "because Windows" but why the fuck.
+ENCODING = "cp932"
+FILENAME = "motion.vmd"
+
 """
-possible_encodings = [
+possible_ENCODINGs = [
     "cp932",
     "euc_jp",
     "euc_jis_2004",
@@ -19,18 +29,107 @@ possible_encodings = [
     "shift_jisx0213",
 ]
 """
-# Why the fuck is this in CP932. The answer is "because Windows" but why the fuck.
-encoding = "cp932"
-filename = "motion.vmd"
-with open(filename, "rb") as f:
+
+BoneFrame = namedtuple('BoneFrame', [
+    'bone_name',
+    'frame_id',
+    'x',
+    'y',
+    'z',
+    'x_rq',
+    'y_rq',
+    'z_rq',
+    'w_rq',
+    'frame_interpo',
+])
+
+FILE_STRUCTURE = [
+    ("version", 30, str),
+    ("model_name", 20, str),
+    ("bone_frames_len", 4, int),
+    [
+        (BoneFrame),
+        ("bone_name", 15, str),
+        ("frame_id", 4, int),
+        ("x", 4, float),
+        ("y", 4, float),
+        ("z", 4, float),
+        ("x_rq", 4, float),
+        ("y_rq", 4, float),
+        ("z_rq", 4, float),
+        ("w_rq", 4, float),
+        ("frame_interpo", 64, str),
+    ],
+]
+
+def convertByteArrayToType(b_arr, t):
+    cleaned_arr = bytearray()
+    #for b in b_arr:
+    #    if b != b'\x00':
+    #        cleaned_arr.add(b)
+    print(b_arr, cleaned_arr)
+
+    if t is str:
+        b_converted = b_arr.strip(b'\x00').decode(ENCODING)
+    elif t is int:
+        b_converted = int.from_bytes(b_arr, byteorder='big', signed=False)
+    elif t is float:
+       b_converted = struct.unpack('>f', b_arr)[0]
+
+    return b_converted
+
+def grabBytes(contents, offset, length):
+    b = contents[offset:offset+length]
+    return b
+
+with open(FILENAME, "rb") as f:
     contents = bytearray(f.read())
+
+    DATA = {}
+    OFFSET = 0
+    PREV_SECTION_NAME = None
+    for section in FILE_STRUCTURE:
+        structure_type = type(section)
+
+        if structure_type == tuple:
+            sec_name, sec_len, sec_type = section
+            #print("Looking at %s of len %d and converting to type %s" % section)
+            b_content = grabBytes(contents, OFFSET, sec_len)
+            b_converted = convertByteArrayToType(b_content, sec_type)
+            print("%s: %s - %s - %s" % (sec_name, b_converted, sec_type, b_content) )
+            OFFSET += sec_len
+        else:
+            struct_type = section[0]
+            print("Using namedtuple:")
+            print(struct_type)
+            eval_string = struct_type.__name__+"("
+            for sub_section in section[1:]:
+                print(sub_section)
+                sec_name, sec_len, sec_type = sub_section
+                print("Looking at %s of len %d and converting to type %s" % sub_section)
+
+                b_content = grabBytes(contents, OFFSET, sec_len)
+                b_converted = convertByteArrayToType(b_content, sec_type)
+
+                print("+++", b_converted, repr(b_converted))
+
+                if sec_type == str:
+                    eval_string += "%s = \"%s\"," % (sec_name, b_converted)
+                else:
+                    eval_string += "%s = %s," % (sec_name, b_converted)
+                OFFSET += sec_len
+            eval_string = eval_string[:-1] + ")"
+            print(eval_string)
+            #print("\x".join("{:02x}".format(ord(c)) for c in eval_string))
+            frame = eval(eval_string)
+            print(frame)
     ver = contents[:30]
     model = contents[30:50]
     both = contents[:50]
 
-    model.decode(encoding)
-    print(ver.decode(encoding), model.decode(encoding))
-    print(both.decode(encoding))
+    model.decode(ENCODING)
+    print(ver.decode(ENCODING), model.decode(ENCODING))
+    print(both.decode(ENCODING))
 
     kf_count = contents[50:54]
     kf_int = int.from_bytes(kf_count, byteorder='big', signed=False)
@@ -64,17 +163,8 @@ with open(filename, "rb") as f:
             f_name, f_len, f_type = field
             print("Grabbing data from %d to %d" % (offset_start+inner_offset, offset_start+inner_offset+f_len))
             b_data = contents[offset_start+inner_offset:offset_start+inner_offset+f_len]
-            if f_type is str:
-                print(b_data)
-                b_converted = b_data.decode(encoding)
-                print(b_converted)
-            elif f_type is int:
-                b_converted = int.from_bytes(b_data, byteorder='big', signed=False)
-            elif f_type is float:
-               b_converted = struct.unpack('>f', b_data)
 
-            frame_data[f_name] = b_converted
-
+            frame_data[f_name] = convertByteArrayToType(b_data, f_type)
             inner_offset += f_len
 
         print(frame_data)
